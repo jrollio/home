fcddd6916316289fc1b3aeeeb32bb0e9e5c22b83
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2023-03-02 14:58:17 +0000

    back to the future

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ SD=$HOME/home
 
 # Source files
 SF="$SD/.alias $SD/.env $SD/.paths"
+UP=$SD/ps1_userpart.txt
 
 # Working directory
 WD=$HOME/.tmptmp
@@ -38,16 +39,25 @@ BC=$(($TFL-$BB))
 # Move dotfiles to home directory
 for file in $SF; do cp -f $file $HOME; done
 
-# Create temp directory
+# Create work directory
 mkdir -p $WD
 
 # Build source components into new target file
 head -n$TC $TF > $WF && cat $IF >> $WF && tail -n$BC $TF >> $WF
 
+# Remove escaped newlines
 sed ':a; N; $!ba; s/\\\n//g' $WF > $WF
+bashrcLen=$(wc -l ${WF} | cut -d' ' -f1)
+usrPartLn=$(grep -nE '^[^#]\s+local\ userpart\=' ${WF} | cut -d':' -f1)
+insertBeg=$((${usrPartLn}-1))
+insertEnd=$((${bashrcLen}-${usrPartLn}))
+# tgtLnNm=$(grep -nE '^[^#].*local\ userpart' ${WF} | cut -d':' -f1)
+# bashStart="$(grep -n '__bash_prompt() {' ${WF} | cut -d':' -f 1)"
+# bashEnd="$(grep -ne '^__bash_prompt$' ${WF} | cut -d':' -f 1)"
 
+head -n$insertBeg $WF > $TF && cat $UP >> $TF && tail -n$insertEnd $WF >> $TF
 
-mv $WF $TF
+# mv $WF $TF
 
 # Cleanup
 rm -rf $SD $WD
