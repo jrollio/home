b643822759f7742f3d8af82d0df97ad0baa4cf24
jonathanroll <75393433+jonathanroll@users.noreply.github.com>
2023-03-03 01:31:31 +0000

    table driven file mods

--- install.sh
+++ install.sh
@@ -1,63 +1,95 @@
 #!/usr/bin/env /bin/bash
 
+set -avx
+
 export PATH=/bin:/usr/bin:$PATH
 
 # Source directory
 SD=$HOME/home
 
 # Source files
-SF="$SD/.alias $SD/.env $SD/.paths"
-UP=$SD/ps1_userpart.txt
+SF=".alias .env .paths"
+
+# Table of changes
+dTbl=$SD/fchgs.txt
+
+for file in $SF; do
+    if [ -f $HOME/$file ]; then
+        #if [[ $(diff -q $HOME/$file $SD/$file > /dev/null 2>&1) -ne 0 ]]; then
+        if [ "$(diff -q $HOME/$file $SD/$file)" ]; then
+            echo "Appending contents of $SD/$file to $HOME/$file"
+            echo "########################################################" >> $HOME/$file
+            echo "#     Content appended from $SD/$file on $(date)" >> $HOME/$file
+            echo "########################################################" >> $HOME/$file
+            cat $SD/$file >> $HOME/$file
+            echo "############### End of Append ######################" >> $HOME/$file
+        fi
+    else
+        cp $SD/$file $HOME
+    fi
+done
 
 # Working directory
 WD=$HOME/.tmptmp
 
-# Filename
-FN=".bashrc"
-
-# Working file
-WF=$WD/$FN.tmp
-
-# File to be modified ("TF - target file")
-TF=$HOME/$FN
-
-# File to be inserted
-IF=$SD/modf.txt
-
-# Target file line count
-TFL=`wc -l $TF | awk -F" " '{ print $1 }'`
-
-# Top cutoff
-TC=$((`grep -n "# Alias definitions."  $TF | awk -F: '{ print $1 }'`-1))
-
-# Bottom boundary
-BB=$((`grep -n "# enable programmable completion features" $TF | awk -F: '{ print $1 }'`-1))
-
-# Bottom cutoff
-BC=$(($TFL-$BB))
-
-# Move dotfiles to home directory
-for file in $SF; do cp -f $file $HOME; done
-
 # Create work directory
 mkdir -p $WD
 
-# Build source components into new target file
-head -n$TC $TF > $WF && cat $IF >> $WF && tail -n$BC $TF >> $WF
+# Process each record in the input table
+IFS=$'\t'; while read -r recid targetFile deltaFile upperMarker lowerMarker; do
+    WF=$WD/$targetFile.tmp
+    TF=$HOME/$targetFile
+    IF=$SD/$deltaFile
 
-# Remove escaped newlines
-sed ':a; N; $!ba; s/\\\n//g' $WF > $WF
-bashrcLen=$(wc -l ${WF} | cut -d' ' -f1)
-usrPartLn=$(grep -nE '^[^#]\s+local\ userpart\=' ${WF} | cut -d':' -f1)
-insertBeg=$((${usrPartLn}-1))
-insertEnd=$((${bashrcLen}-${usrPartLn}))
-# tgtLnNm=$(grep -nE '^[^#].*local\ userpart' ${WF} | cut -d':' -f1)
-# bashStart="$(grep -n '__bash_prompt() {' ${WF} | cut -d':' -f 1)"
-# bashEnd="$(grep -ne '^__bash_prompt$' ${WF} | cut -d':' -f 1)"
+    wc $IF $TF /tmp/home/$targetFile $targetFile
 
-head -n$insertBeg $WF > $TF && cat $UP >> $TF && tail -n$insertEnd $WF >> $TF
+    # Remove escaped newlines
+    sed -i ':a; N; $!ba; s/\\\n//g' $TF
+    #sed ':a; N; $!ba; s/\\\n//g' $TF > $WF
 
-# mv $WF $TF
+    # Target file line count
+    TFL=`wc -l $TF | awk -F" " '{ print $1 }'`
+
+    # Establish line boundaries
+    upperLine=$(grep -nE ${upperMarker} ${TF} | cut -d: -f1 | tail -1)
+    lowerLine=$(grep -nE ${lowerMarker} ${TF} | cut -d: -f1 | head -1)
+
+    # If match was found, then result length will be  > 0...
+    if [[ ${#upperLine} -gt 0 && ${#lowerLine} -gt 0 ]]; then
+
+        # ...so do some simple boundary checks
+        if [[ $upperLine -ge 2 ]]; then
+            headBoundary=$((${upperLine}-1))
+        else
+            headBoundary=$((${upperLine}-0))
+        fi;
+        #if [[ $lowerLine -gt $upperLine ]]; then
+        if [[ $lowerLine -ge $upperLine ]]; then
+            tailBoundary=$((${TFL}-${lowerLine}))
+        else
+            tailBoundary=$((${TFL}-0))
+        fi;
+    else
+        # No match, move to the next record
+        continue
+    fi;
+    
+    # If upper and lower bounds were found, process changes
+    # Build source components into new target file
+	head -n$headBoundary $TF > $WF && cat $IF >> $WF && tail -n$tailBoundary $TF >> $WF
+
+    wc $IF $TF $WF
+
+    # Replace target with updated working file
+    mv $WF $TF
+
+done < $dTbl
+
+# Move dotfiles to home directory
+#for file in $SF; do
+#	/bin/ls -alF $file
+#	cp -f $file $HOME
+#done
 
 # Cleanup
 rm -rf $SD $WD
@@ -65,3 +97,11 @@ rm -rf $SD $WD
 # Manual workaround until automation works
 # chmod u+x $HOME/home/install.sh
 # $HOME/home/install.sh
+
+# -- trial and error ---
+# usrPartLn=$(grep -nE '^[^#]\s+local\ userpart\=' ${WF} | cut -d':' -f1)
+# tgtLnNm=$(grep -nE '^[^#].*local\ userpart' ${WF} | cut -d':' -f1)
+# bashStart="$(grep -n '__bash_prompt() {' ${WF} | cut -d':' -f 1)"
+# bashEnd="$(grep -ne '^__bash_prompt$' ${WF} | cut -d':' -f 1)"
+#head -n$insertBeg $WF > $TF && cat $UP >> $TF && tail -n$insertEnd $WF >> $TF
+# -- trial and error ---
